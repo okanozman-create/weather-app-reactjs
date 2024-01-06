2cf9112763cccf57bcb17837a10a7e32231c5164
okanozman-create <okanozman88@gmail.com>
2024-01-06 21:51:56 +0300

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "weatherappreactjs": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "weatherappreactjs8f8dca4f": {
       "AppClientID": "string",
@@ -8,6 +15,12 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "storage4weatherapp": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,34 +1,185 @@
 import { useEffect, useState } from "react";
 import IMAGES from "./images";
 import toDateFunction from "./toDateFunction";
-import config from "./config.json";
+import configs from "./config.json";
 
-import "@aws-amplify/ui-react/styles.css";
+
+
+
+
+// import "./App.css";
+// import { Amplify } from 'aws-amplify';
+// import { generateClient } from 'aws-amplify/api';
+
+// import { API } from "aws-amplify";
+// import "@aws-amplify/ui-react/styles.css";
+// import {
+//   withAuthenticator,
+//   Button,
+//   Flex,
+//    Heading,
+//   // Image,
+//   Text,
+//   TextField,
+//   View,
+//   Card,
+// } from "@aws-amplify/ui-react";
+
+// import { listNotes } from "./graphql/queries";
+// import {
+//   createNote as createNoteMutation,
+//   deleteNote as deleteNoteMutation,
+// } from "./graphql/mutations";
+
+// import config from './amplifyconfiguration.json';
+// Amplify.configure(config);
+// const client = generateClient();
+
 import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
   Card,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  TextField,
+  View,
+  // WithAuthenticatorProps,
+  withAuthenticator,
 } from "@aws-amplify/ui-react";
+import "./style.css";
+import "@aws-amplify/ui-react/styles.css";
+import { listNotes } from "../src/graphql/queries";
+import {
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
+} from "../src/graphql/mutations";
+// import { CreateNoteInput, Note } from "@/src/API";
+  // import { API, Storage } from 'aws-amplify';
+  // import { Storage } from 'aws-amplify';
+// import API from '@aws-amplify/api'
+// import Storage from '@aws-amplify/storage'
 
 
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+// import awsconfig from "@/src/amplifyconfiguration.json";
+// Amplify.configure(awsconfig);
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
 
 
+const storage = Amplify.Storage;
 
+const client = generateClient();
 
+const apiKey = configs.apiKey;
 
-const apiKey = config.apiKey;
+const App = ({signOut}) =>{
 
-
-
- function App({signOut}) {
+//  function App({signOut}) {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [containerRes, setConatinerRes] = useState([]);
+
+  const [notes, setNotes] = useState([]);
+
+ 
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+  
+  async function fetchNotes() {
+    const apiData = await client.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+    setNotes(notesFromAPI);
+  }
+  
+  // async function createNote(event) {
+  //   const form = event.target;
+  
+  //   event.preventDefault();
+  
+  //   const formData = new FormData(form);
+  
+  //   const data = {
+  //     name: formData.get('name'),
+  //     description: formData.get('description'),
+  //   };
+  
+  //   await client.graphql({
+  //     query: createNoteMutation,
+  //     variables: { input: data },
+  //   });
+  
+  //   await fetchNotes();
+  
+  //   form.reset();
+  // }
+
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image = form.get("image");
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image.name,
+    };
+    if (!!data.image) await storage.put(data.name, image);
+    await client.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+  
+
+
+
+
+
+
+  
+  // async function deleteNote({ id }) {
+  //   const newNotes = notes.filter((note) => note.id !== id);
+  
+  //   setNotes(newNotes);
+  
+  //   await client.graphql({
+  //     query: deleteNoteMutation,
+  //     variables: { input: { id } },
+  //   });
+  // }
+
+
+  async function deleteNote({ id }) {
+    const newNotes = notes.filter((note) => note.id !== id);
+  
+    setNotes(newNotes);
+  
+    await client.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }
+
+
+
+
+
 
   async function fetchData() {
     try {
@@ -82,6 +233,69 @@ const apiKey = config.apiKey;
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
+
+
+    <View className="App">
+      <Heading level={1}>My Notes App</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createNote}>
+        <Flex direction="row" justifyContent="center">
+
+
+
+        <View
+  name="image"
+  as="input"
+  type="file"
+  style={{ alignSelf: "end" }}
+/>
+
+
+
+          <TextField
+            name="name"
+            placeholder="Note Name"
+            label="Note Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Note Description"
+            label="Note Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Note
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Notes</Heading>
+      <View margin="3rem 0">
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
+      </View>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+
+
+
 
 
 
