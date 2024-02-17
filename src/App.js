93dc7a33d266e859d79a0285719cc9606e6d1622
okaozman-create <okanozman88@gmail.com>
2024-02-18 01:32:57 +0300

    some improvements in Backend in AWS and debugging

--- src/App.js
+++ src/App.js
@@ -26,19 +26,37 @@ const App = () => {
       // const res = await fetch(
       //   `${lambdaEndpoint}/myWeatherAppFunction-staging?city=${city}`
       // );
+      // const apiKey = process.env.OPENWEATHER_API_KEY; 
+      // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+      // const res = await fetch(
+      //   "https://kufhnbgos4.execute-api.eu-north-1.amazonaws.com/Prod2",
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({city}),
+      //   }
+      // );
+
+      // const res = await fetch(url, {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // });
 
 
-      const res = await fetch(
-        "https://kufhnbgos4.execute-api.eu-north-1.amazonaws.com/Prod2",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({city}),
-        }
-      );
+      const lambdaEndpoint = 'https://vzgnt19q7c.execute-api.eu-west-1.amazonaws.com/prod1/'; // Replace with your actual Lambda endpoint
+      const url = `${lambdaEndpoint}?city=${city}`;
 
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       const data = await res.json();
 
